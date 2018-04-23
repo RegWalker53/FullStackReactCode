ada035db5b6a7eed5ad18cc4a52b27134aa1d7ca
Reg Walker <reginald.t.walker@gmail.com>
2018-04-23 12:43:41 -0500

    Server at point of auth error

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+console.log('ClientID: ', keys.googleClientID);
+console.log('ClientSecret:', keys.googleClientSecret);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token: ', accessToken);
+      console.log('refresh token: ', refreshToken);
+      console.log('profile: ', profile);
+      console.log('done: ', done);
+    }
+  )
+);
+
+// Used to verify that the server is running
+app.get('/', () => {
+  console.log('this route works');
 });
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+/*
+app.get('/auth/google/callback', (err, user, info) => {
+  console.log('Back inside callback');
+});
+*/
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
